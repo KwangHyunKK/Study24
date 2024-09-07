e5fbace4997fd2b3613fe6afb80524410d8893aa
KwangHyunKK <panghyun35@naver.com>
2024-09-07 04:31:02 +0000

    [CPP][Algorithm][Sturcture] Union-Find

--- CPP17/src/Day240907.hpp
+++ CPP17/src/Day240907.hpp
@@ -39,12 +39,96 @@ namespace d240907
         return 0;
     }
 
+    // # 2. Union-Find
+    class UnionFind
+    {
+    private:
+        vector<int> parent;
+        vector<int> rank; // rank of parent
+
+    public:
+        UnionFind(int n)
+        {
+            parent.resize(n);
+            rank.resize(n, 0);
+
+            int value = 0;
+            std::for_each(begin(parent), end(parent), [&value](int& element){
+                element = value++;
+            });
+        }
+
+        // Find
+        int find(int x)
+        {
+            if(parent[x] != x)parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        void unionSets(int a, int b)
+        {
+            int x = find(a);
+            int y = find(b);
+
+            if(x != y)
+            {
+                if(rank[x] > rank[y])parent[y] = x;
+                else if(rank[x] < rank[y])parent[x] = y;
+                else{
+                    parent[y] = x;
+                    rank[x]++;
+                }
+            }
+            return;
+        }
+    };
+
+    bool checkCycle(int n, vector<pair<int, int>>& edges)
+    {
+        UnionFind uf(n);
+
+        for(const auto& edge : edges)
+        {
+            int u = edge.first;
+            int v = edge.second;
+
+            if(uf.find(u) == uf.find(v))return true;
+
+            uf.unionSets(u, v);
+        }
+        return false;
+    }
+
+    int run2()
+    {
+        int n = 5; // # of node
+        vector<pair<int, int>> edges = {
+            {0, 1}, {1,2}, {2,3}, {3,4}, {4,1}
+        };
+
+        if(checkCycle(n, edges))
+        {
+            std::cout << "사이클 존재\n";
+        }
+        else{
+            std:cout << "No cycle\n";
+        }
+
+        return 0;
+    }
+
 
     int run()
     {
         std::thread t1(run1);
 
         if(t1.joinable())t1.join();
+
+        std::cout << "run1 end. run2 will be started.\n";
+
+        std::thread t2(run2);
+        
+        if(t2.joinable())t2.join();
 
         return 0;
     }
