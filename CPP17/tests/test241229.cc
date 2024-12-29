b251f32447ace762941550f176fb5a800775db74
KwangHyunKK <panghyun35@naver.com>
2024-12-30 00:10:19 +0900

    [CTest] Test code for D241229.hpp

---
+++ CPP17/tests/test241229.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <deque>
+#include <string>
+#include <vector>
+#include <gtest/gtest.h>
+#include <filesystem>
+#include <fstream>
+#include "../src/Day241229.hpp"
+using namespace D241229;
+
+// TEST(Test241229, CalculateHash)
+// {
+//     namespace fs = std::filesystem;
+//     fs::path currentPath = fs::current_path();
+//     std::cout << currentPath << "\n";
+//     std::string str1 = calculateHash("test", currentPath);
+//     std::string str2 = calculateHash("test", currentPath);
+//     EXPECT_EQ(str1, str2);
+// }
+
+TEST(Test241229, EmptyLCS)
+{
+    std::vector<std::string> oldLines = {};
+    std::vector<std::string> newLines = {};
+    auto result = computeLCS(oldLines, newLines);
+    EXPECT_TRUE(result.empty());
+}
+
+TEST(Test241229, AddLine)
+{
+    std::vector<std::string> oldLines = {"Line 1"};
+    std::vector<std::string> newLines = {"Line 1", "Line 2"};
+    auto result = computeLCS(oldLines, newLines);
+    EXPECT_EQ(result[1].first, std::make_pair("+", "Line 2").first);
+    EXPECT_EQ(result[1].second, std::make_pair("+", "Line 2").second);
+}
+
+TEST(Test241229, RemoveLine) {
+    std::vector<std::string> oldLines = {"Line 1", "Line 2"};
+    std::vector<std::string> newLines = {"Line 1"};
+    auto result = computeLCS(oldLines, newLines);
+    EXPECT_EQ(result[1].first, std::make_pair("-", "Line 2").first);
+    EXPECT_EQ(result[1].second, std::make_pair("-", "Line 2").second);
+}
+
+TEST(Test241229, ReplaceLine) {
+    std::vector<std::string> oldLines = {"Line 1"};
+    std::vector<std::string> newLines = {"Line 2"};
+    auto result = computeLCS(oldLines, newLines);
+    EXPECT_EQ(result[0].first, std::make_pair("-", "Line 1").first);
+    EXPECT_EQ(result[0].second, std::make_pair("-", "Line 1").second);
+    EXPECT_EQ(result[1].first, std::make_pair("+", "Line 2").first);
+    EXPECT_EQ(result[1].second, std::make_pair("+", "Line 2").second);
+}
