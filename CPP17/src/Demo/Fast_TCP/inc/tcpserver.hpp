740d0cb7a14d0bfa5d56a84eead6fa8180437cc4
KwangHyunKK <panghyun35@naver.com>
2025-05-01 18:18:23 +0900

    [Demo][Fast_TCP] Upgrade code with epoll

--- CPP17/src/Demo/Fast_TCP/inc/tcpserver.hpp
+++ CPP17/src/Demo/Fast_TCP/inc/tcpserver.hpp
@@ -26,6 +26,25 @@ namespace Demo
             std::mutex queue_mutex_;
             std::condition_variable queue_cv_;
         }; // TcpServer class
+
+        class TcpServerEpoll
+        {
+        public:
+            TcpServerEpoll(int port) : port_(port), running_(false) {}
+            void start();
+            void stop();
+
+        private:
+            void accept_loop();
+            void worker_loop();
+            void set_nonblocking(int sock);
+
+            int port_;
+            int server_sock_;
+            int epoll_fd_;
+            std::atomic<bool> running_;
+            std::vector<std::thread> worker_threads_;
+        }; // TcpServerEpoll Class
     } // namespace Fast_Tcp
 } // namespace Demo
 #endif // _DEMO_FAST_TCP_INC_TCPSERVER_HPP_
--- CPP17/src/Demo/Fast_TCP/src/tcpserver.cpp
+++ CPP17/src/Demo/Fast_TCP/src/tcpserver.cpp
@@ -86,6 +86,110 @@ namespace Demo
                 close(client_sock);
             }
         } // worker_loop
+
+        void TcpServerEpoll::set_nonblocking(int sock)
+        {
+            int flags = fcntl(sock, F_GETFL, 0);
+            fcntl(sock, F_SETFL, flags | O_ONBLOCK);
+        }
+
+        void TcpServerEpoll::start()
+        {
+            server_sock_ = socket(AF_INET, SOCK_STREAM, 0);a
+            assert(server_sock_ >= 0);
+            sockaddr_in addr {};
+            addr.sin_family = AF_INET;
+            addr.sin_addr.s_addr = INADDR_ANY;
+            addr.sin_port = htons(port_);
+
+            int opt = 1;
+            setsockopt(server_sock_, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+            assert(bind(server_sock_, (sockaddr*)&addr,sizeof(addr)) >= 0);
+            assert(listen(server_sock, 128) >= 0);
+            
+            set_nonblocking(server_sock_);
+
+            epoll_fd_ = epoll_create1(0);
+            assert(epoll_fd_ >= 0);
+
+            epoll_event ev {};
+            ev.events = EPOLLIN;
+            ev.data.fd = server_sock_;
+            epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, server_sock_, &ev);
+
+            running_ = true;
+
+            // worker thread start
+            for(int i=0;i<MAX_WORKER_THREADS;++i)
+            {
+                worker_threads_.emplace_back(&TcpServerEpoll::worker_loop, this);
+            }
+        } // start
+
+        void TcpServerEpoll::stop()
+        {
+            running_ = false;
+            close(server_sock_);
+            close(epoll_fd_);
+
+            for(auto& th:worker_threads_)
+            {
+                if(th.joinable())th.join();
+            }
+        } // stop
+
+        void TcpServerEpoll::worker_loop()
+        {
+            constexpr int MAX_EVENTS = 64;
+            epoll_event events[MAX_EVENTS];
+
+            while(running_)
+            {
+                int n = epoll_wait(epoll_fd_, events, MAX_EVENTS, 1000); // timeout 1 second
+                for(int i=0;i<n;++i)
+                {
+                    int fd = events[i].data.fd;
+
+                    if(fd == server_sock_)
+                    {
+                        // new connection
+                        while(true)
+                        {
+                            sockaddr_in client_addr;
+                            socklen_t len = sizeof(client_addr);
+                            int client_sock = accept(server_sock_, (sockaddr*)&client_addr, &len);
+                            if(client_sock < 0)break;
+
+                            set_nonblocking(client_sock);
+
+                            epoll_event ev{};
+                            ev.events = EPOLLIN | EPOLLET; // Edge triggered
+                            ev.data.fd = client_sock;
+                            epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, client_sock, &ev);
+                        }
+                    }
+                    else
+                    {
+                        // Data receive
+                        char buffer[BUFFER_SIZE];
+                        while(true)
+                        {
+                            int bytes = recv(fd, buffer, sizeof(buffer), 0);
+                            if(bytes <= 0)
+                            {
+                                if(errno !+ EAGAIN && errno != EWOULDBLOCK)
+                                {
+                                    close(fd);
+                                }
+                                break;
+                            }
+                            send(fd, buffer, bytes, 0); // echo
+                        }
+                    }
+                }
+            }
+        } // worker_loop
+
     } // namespace Fast_Tcp
 } // namespace Demo
 
