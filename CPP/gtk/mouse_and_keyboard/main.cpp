24e1eafab72b76480146f32b33c24a82196961a7
KwangHyunKK <panghyun35@naver.com>
2025-08-12 00:15:47 +0900

    [C++][GTK4] App with gray background

--- CPP/gtk/mouse_and_keyboard/main.cpp
+++ CPP/gtk/mouse_and_keyboard/main.cpp
@@ -1,10 +1,26 @@
 #include <gtk/gtk.h>
+#include <cmath>
+
+static void draw(GtkDrawingArea*, cairo_t* cr, int w, int h, gpointer)
+{
+    // a dark gray background
+    cairo_set_source_rgb(cr, 0.12, 0.13, 0.16);
+    cairo_rectangle(cr, 0, 0, w, h);
+    cairo_fill(cr);
+}
 
 static void activate(GtkApplication* app, gpointer)
 {
     GtkWidget* win = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(win), "Step1 - Hello GTK4");
     gtk_window_set_default_size(GTK_WINDOW(win), 800, 600);
+
+    GtkWidget* area = gtk_drawing_area_new();
+    gtk_widget_set_hexpand(area, true);
+    gtk_widget_set_vexpand(area, true);
+    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(area), draw, nullptr, nullptr);
+
+    gtk_window_set_child(GTK_WINDOW(win), area);
     gtk_window_present(GTK_WINDOW(win));
 }
 
